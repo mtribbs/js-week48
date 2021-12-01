0fd4fb7b1308e5b70b934ca4fedec21d6639c426
mtribbs <89516065+mtribbs@users.noreply.github.com>
2021-12-01 13:55:47 +0100

    array

--- arrays/script.js
+++ arrays/script.js
@@ -20,6 +20,9 @@ const dog3 = "Doris";
 //const years = new Array(1994, 1985, 2015, 2020);
 //different way of creating an array, not as common though
 
+
+
+
 const dogs = ["Tore", "Clementine", "Doris"];
 console.log(dogs);
 
@@ -37,4 +40,39 @@ console.log(dogs[dogs.length - 1]);
 // can mutate arrays
 dogs[0] = "Trasan";
 console.log(dogs);
+
+// const - only primitive values cannot be changed
+
+// this does not work
+// dogs = ['Bob', 'Fido'];
+
+
+// an array can hold different values
+
+const dogName = "Tore";
+const tore = [dogName, "Miniature Bull Terrier", 2021 - 2018, "He likes sleeping in his fluffy bed", dogs];
+
+
+console.log(tore);
+console.log(tore.length);
+console.log(tore[1]);
+
+
+
+//function to calcultate age
+const calcAge = function(birthYear) {
+    return 2021 - birthYear;
+};
+
+// an array with birth years
+const yearOfBirth = [1999, 1985, 2001, 2016, 1978];
+
+// console.log(calcAge(yearOfBirth));
+// js does not know how to solve this
+
+// cannot do it with the whole array, but can do it with one element in an array
+// need to do one element at a time
+const firstAge = calcAge(yearOfBirth[0]);
+const secondAge = calcAge(yearOfBirth[3]);
+console.log(firstAge, secondAge);
 
