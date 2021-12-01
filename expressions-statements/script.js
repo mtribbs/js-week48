32aa56f881186d125bc6e0eb7e3e639872e2b530
mtribbs <89516065+mtribbs@users.noreply.github.com>
2021-12-01 10:39:49 +0100

    statements expressions

---
+++ expressions-statements/script.js
@@ -0,0 +1,25 @@
+
+//STATEMENTS & EXPRESSIONS
+
+// EXPRESSION
+// a piece of code that will produce a value
+// 3+4 is an expression, it produces a value (7)
+// 1992 is also an expression, it's a value
+// boolean, true && false && !false, is an expression, it produces a boolean value
+
+
+// STATEMENT
+// a bigger piece of code that is executed
+// does not produce a value by itself
+// made up by expressions, smaller pieces
+// statement - sentence, expressions - words 
+
+// we write code as sequences of actions, and those actions are statements
+// e.g. if/else statement
+
+if (23>10) {
+    const str = "23 is bigger";
+}
+// the if/else block is a statement, it does not produce a value, it performs an action
+// the string "23 is bigger" is an expression, it has a value
+// statements end with ;
